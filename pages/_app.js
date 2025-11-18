992d5f65c2ba5dda1ca5fcf0a59eeb3a579906c5
aditya-tn <ekinerjaaditya@gmail.com>
2025-11-18 12:54:52 +0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,6 @@
 // pages/_app.js - Tambahkan NotificationProvider DAN Viewport Meta
 import { PlaylistProvider } from '../contexts/PlaylistContext';
 import { NotificationProvider } from '../contexts/NotificationContext';
-import NotificationContainer from '../components/NotificationContainer';
 import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
